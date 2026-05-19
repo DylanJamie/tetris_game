7fd3ad7ba3bbdfbc0cfc84cb3757ba1fc01926d9
DylanJamie <dylanjjboles@gmail.com>
2026-05-19 01:53:59 -0400

    display added or window

--- main.py
+++ main.py
@@ -4,15 +4,39 @@
 # main python file for tetris in pygame'
 
 # Imports
+import pygame
+import sys
+import random
 
-# Main function
-def main():
-    pass
+# Set up the constraints
+pygame.init()
+X = 1400
+Y = 700
+screen = pygame.display.set_mode((X, Y))
 
-# End of main
+# Load
+LIGHT_BLUE = (173, 216, 230)
+DARK_GRAY  = (40,   40,  40)
+
+# Main while loop
+running = True
+while running == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+
+    # Fill the screen with Light blue
+    screen.fill(LIGHT_BLUE)
+
+    # Update the display
+    pygame.display.flip()
+    
+    # Logic for Dropping the blocks
+
+# End of main loop
 #
-if __name__=="__main__":
-    main()
-#
+pygame.quit()
+sys.exit()
 # End of File
 
