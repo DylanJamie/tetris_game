33bcfb95a6754e95eca7dcce6f11ebd52a64d268
DylanJamie <dylanjjboles@gmail.com>
2026-05-19 01:26:46 -0400

    Initial Tetris commit

---
+++ main.py
@@ -0,0 +1,18 @@
+## main.py
+#
+
+# main python file for tetris in pygame'
+
+# Imports
+
+# Main function
+def main():
+    pass
+
+# End of main
+#
+if __name__=="__main__":
+    main()
+#
+# End of File
+
