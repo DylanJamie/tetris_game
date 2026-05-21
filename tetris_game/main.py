6c2135e4cb41a4fff46c811053f30314b952a0f9
DylanJamie <dylanjjboles@gmail.com>
2026-05-21 02:21:51 -0400

    starting to add matrix grid

--- tetris_game/main.py
+++ tetris_game/main.py
@@ -12,19 +12,15 @@ import src.utils as utils
 
 # BLock movement
 MOVE_RATE  = 20
-BLOCK_SIZE = 20
 
 # Continuous falling speed
 # 500 ms, make the block drop
 BLOCK_FALL_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(BLOCK_FALL_EVENT, 500)
 
-# Grid for the game
-def draw_grid(screen):
-    for x in range(0, utils.X, BLOCK_SIZE):
-        for y in range(0, utils.Y, BLOCK_SIZE):
-            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(screen, utils.GRID_COLOR, rect, 1)
+# Make a new function to store a new block ath the top center
+def spawn_new_block():
+    return (utils.X // 2) - (utils.Y // 2), 0
 
 # Main Function
 def main():
@@ -32,11 +28,30 @@ def main():
     SCREEN = pygame.display.set_mode((utils.X, utils.Y))
     CLOCK = pygame.time.Clock()
     pygame.display.set_caption("Tetris")
-    
-    # block prop
-    x_pos = utils.X // 2 - (BLOCK_SIZE // 2)
-    y_pos = 0
-    
+
+    # Creating a 20x20 grid martix to store the landed blocks
+    # Rows = 400 // 20 = 20 | cols = 400 // 20 = 20
+    grid_rows = utils.Y // utils.BLOCK_SIZE
+    grid_cols = utils.X // utils.BLOCK_SIZE
+
+    # Create an empty list for the grid rows
+    grid = []
+
+    # Loop for all the rows on the screen
+    for row in range(grid_rows):
+        new_row = []
+
+        # fill this with empty columns
+        for col in range(grid_cols):
+            new_row.append(None)
+
+        # add the completed row to our main grid
+        grid.append(new_row)
+
+    # this makes every block spawn in the center 0
+    # the spawn new block funct returns a tuple of coords
+    x_pos, y_pos = spawn_new_block()
+        
     # Main while loop
     running = True
     while running == True:
@@ -44,7 +59,15 @@ def main():
         SCREEN.fill(utils.TETRIS_BLUE)
 
         # Draw Grid
-        draw_grid(SCREEN)
+        utils.draw_grid(SCREEN)
+
+        # Draw all the static blocks that have already been stacked
+        for r in range(grid_rows):
+            for c in range(grid_cols):
+                if grid[r][c] is not None:
+                    # render the locked block using the color stored in the grid
+                    locked_rect = pygame.Rect(c * utils.BLOCK_SIZE, r * utils.BLOCK_SIZE, utils.BLOCK_SIZE, utils.BLOCK_SIZE)
+                    pygame.draw.rect(SCREEN, grid[r][c], locked_rect)
 
         # Handle the inputs
         for event in pygame.event.get():
@@ -54,24 +77,26 @@ def main():
 
             # continuous falling event falling from timer    
             if event.type == BLOCK_FALL_EVENT:
-                if y_pos < utils.Y - BLOCK_SIZE:
-                    y_pos += MOVE_RATE
+                if y_pos < utils.Y - utils.BLOCK_SIZE:
+                    next_y = y_pos + MOVE_RATE
+                    grid_x = x_pos // utils.BLOCK_SIZE
+                    next_grid_y = next_y // utils.BLOCK_SIZE
 
             # Snap to the bottom when hitting the bottom floor
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
-                    y_pos = utils.Y - BLOCK_SIZE
+                    y_pos = utils.Y - utils.BLOCK_SIZE
 
                 # Make boundry checks so the blocks don't go off screen
                 if event.key == pygame.K_RIGHT:
-                    if x_pos < utils.X - BLOCK_SIZE:
+                    if x_pos < utils.X - utils.BLOCK_SIZE:
                         x_pos += MOVE_RATE
                 if event.key == pygame.K_LEFT:
                     if x_pos > 0:
                         x_pos -= MOVE_RATE
                     
         # Define and draw active falling piece
-        current_block = pygame.Rect(x_pos, y_pos, BLOCK_SIZE, BLOCK_SIZE)
+        current_block = pygame.Rect(x_pos, y_pos, utils.BLOCK_SIZE, utils.BLOCK_SIZE)
         pygame.draw.rect(SCREEN, utils.LIME_GREEN, current_block)
 
         # Update the display
--- tetris_game/src/utils.py
+++ tetris_game/src/utils.py
@@ -10,6 +10,7 @@ import sys
 # Set up the constraints
 X = 400
 Y = 400
+BLOCK_SIZE = 20
 
 # Define the screen
 SCREEN = pygame.display.set_mode((X, Y))
@@ -26,3 +27,10 @@ GRID_COLOR  = ( 38,  56, 138)
 LIME_GREEN  = ( 50, 205,  50)
 
 ## Functions ##
+
+# Grid for the game
+def draw_grid(screen):
+    for x in range(0, X, BLOCK_SIZE):
+        for y in range(0, Y, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, GRID_COLOR, rect, 1)
